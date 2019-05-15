9fa888232826b1b025b8e2ebd6c7837a56c41a4a
UsuallyChris <schoonc@gmail.com>
2019-05-15 15:13:51 -0700

    Added basic rendering of todos and added axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,3 +17,27 @@ const list = [
     "body": "Learn django."
   },
 ]
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.list.map( item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
