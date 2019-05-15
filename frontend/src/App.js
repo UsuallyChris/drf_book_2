65707cea9af46f6105d06569a445631836c5b132
UsuallyChris <schoonc@gmail.com>
2019-05-15 14:56:54 -0700

    Deleted default content in App.js and added mock up of data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const list = [
+  {
+    "id": 1,
+    "title": "1st todo",
+    "body": "Learn django."
+  },
+  {
+    "id": 2,
+    "title": "2nd todo",
+    "body": "Learn django."
+  },
+  {
+    "id": 3,
+    "title": "3rd todo",
+    "body": "Learn django."
+  },
+]
